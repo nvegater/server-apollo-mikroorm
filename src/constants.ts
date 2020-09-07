8045886211486c766adb108167aa5287cc28738b
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-09-07 14:52:42 +0200

    Added post entity and typeORM structure

---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const _prod_ = process.env.NODE_ENV === 'production'
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello again its me now what")
+import {MikroORM} from "@mikro-orm/core"
+import {_prod_} from "./constants";
+import {Post} from "./entities/Post";
+
+const main = async () => {
+    const orm = MikroORM.init({
+        entities: [Post],
+        dbName: "lireddit",
+        user: "admin-p00920345",
+        password: "Gibson.123",
+        debug: !_prod_,
+        type: "postgresql"
+    })
+
+}
+
+main();
