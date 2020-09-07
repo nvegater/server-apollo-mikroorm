56ee0c1890e25b386fbe34dfd95f137369547f4c
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-09-07 16:46:47 +0200

    Created Dummy Graphql playground

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import {MikroORM} from "@mikro-orm/core"
 import mikroConfig from "./mikro-orm.config"
-import {Post} from "./entities/Post";
+import express from "express";
+import {ApolloServer} from "apollo-server-express";
+import {buildSchema} from "type-graphql";
+import {HelloResolver} from "./resolvers/hello";
 
 const main = async () => {
+    // ORM Config
     const orm = await MikroORM.init(mikroConfig) // connect to DB
     await orm.getMigrator().up();
-    const post = orm.em.create(Post, {title: "First Post"})
-    await orm.em.persistAndFlush(post);
+    // Server config
+    const app = express();
 
-    const posts = await orm.em.find(Post, {});
-    console.log(orm, __dirname, post, posts)
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    })
+
+
+    apolloServer.applyMiddleware({app})
+    app.listen(4000, () => {
+        console.log("Server started in localhost: 4000");
+    })
 }
 
 main().catch((err)=>{
---
+++ src/resolvers/hello.ts
@@ -0,0 +1,8 @@
+import {Query, Resolver} from "type-graphql"
+@Resolver()
+export class HelloResolver {
+    @Query(()=>String)
+    hello() {
+        return "Hello world"
+    }
+}
