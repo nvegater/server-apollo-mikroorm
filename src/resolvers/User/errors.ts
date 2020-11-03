b52e69b0e96389e89ab0ed565c5cf414fc48103b
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-11-03 11:16:46 +0100

    refactor error handling for better readability and abstractions.

--- src/resolvers/User/errors.ts
+++ src/resolvers/User/errors.ts
@@ -1,4 +1,5 @@
 import {Field, ObjectType} from "type-graphql";
+import {CredentialsInputs} from "./arguments";
 
 /**
  * This file is for error types of resolvers related to an user
@@ -10,3 +11,107 @@ export class FieldError {
     @Field()
     message: string;
 }
+enum Fields {
+    username = "username",
+    email = "email",
+    password = "password",
+}
+// User input dependant errors
+const usernameMissingInputError: FieldError = {
+    field: Fields.username,
+    message: "User already exists"
+}
+const usernameTooShortInputError: FieldError = {
+    field: Fields.username,
+    message: "username too short"
+}
+const emailIsInvalidInputError: FieldError = {
+    field: Fields.email,
+    message: "email is invalid"
+}
+const emailIsMissingInputError: FieldError = {
+    field: Fields.email,
+    message: "email is missing"
+}
+const passwordTooShortInputError: FieldError = {
+    field: Fields.password,
+    message: "try a better password" // Dont give the reason of bad password
+}
+const passwordMissingInputError: FieldError = {
+    field: Fields.password,
+    message: "password missing" // Dont give the reason of bad password
+}
+/*
+General input checks for login and registration:
+*   Username given?
+*   Email given?. Email valid?
+*   password given?
+* */
+const validateInputs = (username: string, email: string, password: string): FieldError[] => {
+    let inputErrors: FieldError[] = [];
+    const USERNAME_GIVEN = username.length > 0;
+    if (!USERNAME_GIVEN) {
+        inputErrors.push(usernameMissingInputError)
+    }
+    const EMAIL_GIVEN = email.length > 0;
+    if (!EMAIL_GIVEN) {
+        inputErrors.push(emailIsMissingInputError)
+    } else {
+        const EMAIL_VALID = email.includes('@') && email.includes('.');
+        if (!EMAIL_VALID) {
+            inputErrors.push(emailIsInvalidInputError)
+        }
+    }
+    const PASSWORD_GIVEN = password.length > 0;
+    if (!PASSWORD_GIVEN) {
+        inputErrors.push(passwordMissingInputError)
+    }
+
+    return inputErrors;
+}
+
+// General Checks
+export const validateInputsLogin = (inputs: CredentialsInputs): FieldError[] => {
+    return validateInputs(inputs.username, inputs.email, inputs.password);
+}
+// General checks + Username password requirements
+export const validateInputsRegister = (inputs: CredentialsInputs): FieldError[] => {
+    let inputErrors: FieldError[] = validateInputs(
+        inputs.username,
+        inputs.email,
+        inputs.password);
+
+    const USERNAME_SHORT = inputs.username.length <= 2;
+    if (USERNAME_SHORT) {
+        inputErrors.push(usernameTooShortInputError)
+    }
+
+    if (inputs.password.length <= 2) {
+        inputErrors.push(passwordTooShortInputError)
+    }
+    return inputErrors;
+}
+// Database dependant errors
+export const loginErrors: FieldError[] = [
+    {
+        field: Fields.username,
+        message: "-"
+    },
+    {
+        field: Fields.email,
+        message: "-"
+    },
+    {
+        field: Fields.password,
+        message: "the username or password is invalid"
+    }];
+
+export const usernameInUseError: FieldError = {
+    field: Fields.username,
+    message: "User already exists"
+}
+export const emailInUseError: FieldError = {
+    field: Fields.email,
+    message: "That email is already in use"
+}
+
--- src/resolvers/User/user.ts
+++ src/resolvers/User/user.ts
@@ -3,105 +3,16 @@ import {ApolloORMContext} from "../../types";
 import {User} from "../../entities/User";
 import argon2 from 'argon2'
 import {UserResponse} from "./userResponse";
-import {FieldError} from "./errors";
+import {
+    FieldError,
+    emailInUseError,
+    usernameInUseError,
+    loginErrors,
+    validateInputsLogin,
+    validateInputsRegister
+} from "./errors";
 import {CredentialsInputs} from "./arguments";
 import {SessionCookieName} from "../../redis-config";
-
-const validateInputsRegister = (inputs: CredentialsInputs): FieldError[] => {
-    let inputErrors: FieldError[] = [];
-
-    const USERNAME_GIVEN = inputs.username.length > 0;
-    const USERNAME_SHORT = inputs.username.length <= 2;
-
-    if (USERNAME_GIVEN && USERNAME_SHORT) {
-        inputErrors.push({
-            field: Object.keys(inputs)[0],
-            message: "username too short"
-        })
-    }
-
-    if (!USERNAME_GIVEN) {
-        inputErrors.push({
-            field: Object.keys(inputs)[0],
-            message: "username missing"
-        })
-    }
-
-    const email = inputs.email;
-    const EMAIL_GIVEN = email.length > 0;
-    const EMAIL_VALID = email.includes('@') && email.includes('.');
-
-    if (EMAIL_GIVEN && !EMAIL_VALID) {
-        inputErrors.push({
-            field: Object.keys(inputs)[1],
-            message: "email is invalid"
-        })
-    }
-
-    if (!EMAIL_GIVEN) {
-        inputErrors.push({
-            field: Object.keys(inputs)[1],
-            message: "email missing"
-        })
-    }
-
-    const PASSWORD_GIVEN = inputs.password.length > 0;
-    const PASSWORD_SHORT = inputs.password.length <= 2;
-
-    if (PASSWORD_GIVEN && PASSWORD_SHORT) {
-        inputErrors.push({
-            field: Object.keys(inputs)[2],
-            message: "try a better password"
-        })
-    }
-
-    if (!PASSWORD_GIVEN) {
-        inputErrors.push({
-            field: Object.keys(inputs)[2],
-            message: "password missing"
-        })
-    }
-    return inputErrors;
-}
-
-const validateInputsLogin = (inputs: CredentialsInputs): FieldError[] => {
-    let inputErrors: FieldError[] = [];
-
-    const USERNAME_GIVEN = inputs.username.length > 0;
-    if (!USERNAME_GIVEN) {
-        inputErrors.push({
-            field: Object.keys(inputs)[0],
-            message: "username missing"
-        })
-    }
-
-    const email = inputs.email;
-    const EMAIL_GIVEN = email.length > 0;
-    const EMAIL_VALID = email.includes('@') && email.includes('.');
-
-    if (EMAIL_GIVEN && !EMAIL_VALID) {
-        inputErrors.push({
-            field: Object.keys(inputs)[1],
-            message: "email is invalid"
-        })
-    }
-
-    if (!EMAIL_GIVEN) {
-        inputErrors.push({
-            field: Object.keys(inputs)[1],
-            message: "email missing"
-        })
-    }
-
-    const PASSWORD_GIVEN = inputs.password.length > 0;
-    if (!PASSWORD_GIVEN) {
-        inputErrors.push({
-            field: Object.keys(inputs)[1],
-            message: "password missing"
-        })
-    }
-    return inputErrors;
-}
 
 @Resolver()
 export class UserResolver {
@@ -121,39 +32,24 @@ export class UserResolver {
         @Arg("options") inputArgs: CredentialsInputs,
         @Ctx() {req, postgresORM}: ApolloORMContext
     ): Promise<UserResponse> {
-
         const inputErrors: FieldError[] = validateInputsRegister(inputArgs);
-
         if (inputErrors.length > 0) {
             return {errors: inputErrors}
         }
-
-        const hashedPassword = await argon2.hash(inputArgs.password)
-
-        const userExists = await postgresORM.findOne(User, {username: inputArgs.username});
-        if (userExists) {
-            inputErrors.push({
-                field: Object.keys(inputArgs)[0],
-                message: "User already exists"
-            })
-            return {errors: inputErrors}
+        const userWithUsernameExists: User | null = await postgresORM.findOne(User, {username: inputArgs.username});
+        if (userWithUsernameExists) {
+            return {errors: inputErrors.concat(usernameInUseError)}
         } else {
-            const emailExists = await postgresORM.findOne(User, {email: inputArgs.email});
-            if (emailExists){
-                inputErrors.push({
-                    field: Object.keys(inputArgs)[1],
-                    message: "That email is already in use"
-                })
-                return {errors: inputErrors}
+            const userWithEmailExists: User | null = await postgresORM.findOne(User, {email: inputArgs.email});
+            if (userWithEmailExists) {
+                return {errors: inputErrors.concat(emailInUseError)}
             } else {
                 const user = postgresORM.create(User, {
                     username: inputArgs.username,
                     email: inputArgs.email,
-                    password: hashedPassword
+                    password: await argon2.hash(inputArgs.password)
                 });
                 await postgresORM.persistAndFlush(user);
-                console.log("User after register: ", user)
-                // Login right after registering.
                 req.session!.userId = user.id;
                 return {user: user}
             }
@@ -165,32 +61,21 @@ export class UserResolver {
         @Arg("options") inputArgs: CredentialsInputs,
         @Ctx() {req, postgresORM}: ApolloORMContext
     ): Promise<UserResponse> {
-
         const inputErrors: FieldError[] = validateInputsLogin(inputArgs);
         if (inputErrors.length > 0) {
             return {errors: inputErrors}
         }
-
         const user: User | null = await postgresORM.findOne(User, {username: inputArgs.username})
-        // if an user is returned, verify given password.
-        const userPassMatch = user !== null ? await argon2.verify(user.password, inputArgs.password) : false;
-
-        if (!user || !userPassMatch) {
-            inputErrors.push({
-                field: Object.keys(inputArgs)[0],
-                message: "-"
-            })
-            inputErrors.push({
-                field: Object.keys(inputArgs)[1],
-                message: "the username or password is invalid"
-            })
-            return {errors: inputErrors}
+        if (!user) {
+            return {errors: inputErrors.concat(loginErrors)}
         } else {
-            // Us/Pass valid --> Client deserves an id.
-            // Access Session object in request header.
-            // Start a unique session by setting an id in the request header
-            req.session!.userId = user.id;
-            return {user: user}
+            const userPassMatch = await argon2.verify(user.password, inputArgs.password);
+            if (!userPassMatch) {
+                return {errors: inputErrors.concat(loginErrors)}
+            } else {
+                req.session!.userId = user.id;
+                return {user: user}
+            }
         }
     }
 
