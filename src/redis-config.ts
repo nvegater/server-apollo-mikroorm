2a9272d90625f51e4d82f71fbd4cc3ff5da7aeb9
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-11-04 10:44:41 +0100

    cleanup redis config

--- src/redis-config.ts
+++ src/redis-config.ts
@@ -5,7 +5,7 @@ import {RedisStore} from "connect-redis";
 import {Redis} from "ioredis";
 import {SessionOptions} from "express-session";
 
-export const redisCookieConfig: CookieOptions = {
+const redisCookieConfig: CookieOptions = {
     maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
     // Its in MS but I want it in years. convert to years.
     // Therefore: 1000ms = 1s * (60 sec = 1min) * 1 hour * 1day * 1 year * 10 = 10 years
@@ -14,24 +14,18 @@ export const redisCookieConfig: CookieOptions = {
     sameSite: 'lax', // csrf related.
     secure: _prod_ // cookie only works in https when we are in production
 };
-
-export const SessionCookieName = '_qid';
-
-export const generateUuidv4 = (_req: Request) => {
+const generateUuidv4 = (_req: Request) => {
     return uuidv4()
 };
 
-export const generateRedisStore = (redisStore: RedisStore, redisClient: Redis) => {
-    return new redisStore({
-        client: redisClient,
-        disableTouch: true,
-    })
-};
-
+export const SessionCookieName = '_qid';
 export const generateRedisSession: (store:RedisStore, redis:Redis) => SessionOptions = (store,redis)=> ({
     name: SessionCookieName,
     genid: generateUuidv4,
-    store: generateRedisStore(store, redis),
+    store: new store({
+        client: redis,
+        disableTouch: true,
+    }),
     cookie: redisCookieConfig,
     secret: 'alsuehfnvieuhfuhkdjhfuie', // TODO sign cookie with env variable
     resave: false,
