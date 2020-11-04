8e9f1f66c405ca8be4df6ac02cd5214224143370
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-11-04 10:40:52 +0100

    refactor redis config and cleanup server start

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,43 @@
+import express, {Express, RequestHandler} from "express";
+
+import cors from "cors"
+import {corsConfig} from "./express-config";
+
+import Redis from 'ioredis';
+import {generateRedisSession} from "./redis-config";
+import session from 'express-session';
+import connectRedis, {RedisStore} from 'connect-redis';
+
 import {MikroORM} from "@mikro-orm/core"
 import mikroPostgresConfiguration from "./mikro-orm.config"
-import express, {Express} from "express";
+
 import {ApolloServer} from "apollo-server-express";
 import {ApolloServerExpressConfig} from "apollo-server-express/src/ApolloServer";
-import Redis from 'ioredis';
-import session, {SessionOptions} from 'express-session';
-import connectRedis, {RedisStore} from 'connect-redis';
-import {generateRedisStore, generateUuidv4, redisCookieConfig, SessionCookieName} from "./redis-config";
-import cors from "cors"
 import {apolloMiddlewareConfig, buildApolloSchemas, buildCustomContext, devMode} from "./apollo-config";
-import {corsConfig} from "./express-config";
 
 
 const start_server = async () => {
 
     const app:Express = express();
-    app.use(cors(corsConfig))
+
+    const corsRequestHandler:RequestHandler = cors(corsConfig);
+    app.use(corsRequestHandler)
 
     const redisStore: RedisStore = connectRedis(session)
     const redisClient = new Redis()
-    // TODO Move this to Redis config.
-    const sessionOptions: SessionOptions = {
-        name: SessionCookieName,
-        genid: generateUuidv4,
-        store: generateRedisStore(redisStore, redisClient),
-        cookie: redisCookieConfig,
-        secret: 'alsuehfnvieuhfuhkdjhfuie', // TODO sign cookie with env variable
-        resave: false,
-    };
-
-    app.use(session(sessionOptions));
+    const redisRequestHandler:RequestHandler = session(generateRedisSession(redisStore, redisClient));
+    app.use(redisRequestHandler);
 
     const postgresORM: MikroORM = await MikroORM.init(mikroPostgresConfiguration);
     await postgresORM.getMigrator().up();
-
     const apolloConfig: ApolloServerExpressConfig = {
         schema: await buildApolloSchemas(),
         context: ({req, res})=>
             buildCustomContext({req, res, orm:postgresORM, redisContext:redisClient}),
         playground: devMode,
     };
-
     new ApolloServer(apolloConfig)
         .applyMiddleware(apolloMiddlewareConfig(app))
-
 
     app.listen(4000, () => {
         console.log("Server started in localhost: 4000");
--- src/redis-config.ts
+++ src/redis-config.ts
@@ -3,8 +3,9 @@ import {_prod_} from "./constants";
 import {v4 as uuidv4} from "uuid";
 import {RedisStore} from "connect-redis";
 import {Redis} from "ioredis";
+import {SessionOptions} from "express-session";
 
-export const redisCookieConfig:CookieOptions = {
+export const redisCookieConfig: CookieOptions = {
     maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
     // Its in MS but I want it in years. convert to years.
     // Therefore: 1000ms = 1s * (60 sec = 1min) * 1 hour * 1day * 1 year * 10 = 10 years
@@ -16,13 +17,22 @@ export const redisCookieConfig:CookieOptions = {
 
 export const SessionCookieName = '_qid';
 
-export const generateUuidv4 = (_req:Request) => {
+export const generateUuidv4 = (_req: Request) => {
     return uuidv4()
 };
 
-export const generateRedisStore = (redisStore:RedisStore,redisClient:Redis) => {
+export const generateRedisStore = (redisStore: RedisStore, redisClient: Redis) => {
     return new redisStore({
         client: redisClient,
         disableTouch: true,
     })
 };
+
+export const generateRedisSession: (store:RedisStore, redis:Redis) => SessionOptions = (store,redis)=> ({
+    name: SessionCookieName,
+    genid: generateUuidv4,
+    store: generateRedisStore(store, redis),
+    cookie: redisCookieConfig,
+    secret: 'alsuehfnvieuhfuhkdjhfuie', // TODO sign cookie with env variable
+    resave: false,
+});
