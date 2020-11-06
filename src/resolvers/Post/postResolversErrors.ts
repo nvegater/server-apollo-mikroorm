2eed1f33893b7ae02509dc1bbd54f9b7fd8add95
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-11-06 18:17:48 +0100

    validate post inputs

--- src/resolvers/Post/postResolversErrors.ts
+++ src/resolvers/Post/postResolversErrors.ts
@@ -8,8 +8,20 @@ const userNotLoggedInError:FieldError = {
     field: Fields.creator,
     message: "User not logged in"
 }
+
+const emptyTitleError:FieldError = {
+    field:Fields.title,
+    message: "Your post has no title"
+}
+
+const emptyTextError:FieldError = {
+    field:Fields.text,
+    message: "Your post is empty"
+}
 const postResolversErrors = {
-    userNotLoggedInError
+    userNotLoggedInError,
+    emptyTextError,
+    emptyTitleError
 }
 
 export default postResolversErrors
--- src/resolvers/Post/postResolversInputs.ts
+++ src/resolvers/Post/postResolversInputs.ts
@@ -1,5 +1,6 @@
 import {Field, InputType} from "type-graphql";
 import {FieldError} from "../User/userResolversOutputs";
+import postResolversErrors from "./postResolversErrors";
 
 @InputType()
 export class CreatePostInputs {
@@ -10,10 +11,18 @@ export class CreatePostInputs {
 }
 
 
-export const validateCreatePostInputs:(inputs:CreatePostInputs)=>FieldError[] =
-    (inputs) =>{
-        console.log(inputs)
+export const validateCreatePostInputs: (inputs: CreatePostInputs) => FieldError[] =
+    (inputs) => {
         let inputErrors: FieldError[] = [];
-        // TODO validate inputs
+        const {title, text} = inputs;
+
+        const TITLE_GIVEN = title.length > 0;
+        if (!TITLE_GIVEN) {
+            inputErrors.push(postResolversErrors.emptyTitleError)
+        }
+        const TEXT_GIVEN = text.length > 0;
+        if (!TEXT_GIVEN) {
+            inputErrors.push(postResolversErrors.emptyTextError)
+        }
         return inputErrors
-}
+    }
